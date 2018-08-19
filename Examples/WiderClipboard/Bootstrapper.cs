181d33ad02dd590233e75842b194e9f4bd62b9f3
Mark Kromis <mkromis@outlook.com>
2018-08-19 13:14:14 -0400

    Initial tutorial project

---
+++ Examples/WiderClipboard/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using WiderClipboard.Views;
+using System.Windows;
+using Prism.Modularity;
+using Autofac;
+using Prism.Autofac;
+using Wider.Core;
+
+namespace WiderClipboard
+{
+    class Bootstrapper : WiderBootstrapper
+    {
+        //protected override DependencyObject CreateShell()
+        //{
+        //    return Container.Resolve<MainWindow>();
+        //}
+
+        //protected override void InitializeShell()
+        //{
+        //    Application.Current.MainWindow.Show();
+        //}
+
+        //protected override void ConfigureModuleCatalog()
+        //{
+        //    var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+        //    //moduleCatalog.AddModule(typeof(YOUR_MODULE));
+        //}
+    }
+}
