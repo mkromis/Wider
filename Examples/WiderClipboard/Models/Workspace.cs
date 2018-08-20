1b651798af31e6f4de546cea1553094575dfaed6
Mark Kromis <mkromis@outlook.com>
2018-08-19 23:06:15 -0400

    add simple menu update with refresh.

--- Examples/WiderClipboard/Models/Workspace.cs
+++ Examples/WiderClipboard/Models/Workspace.cs
@@ -1,6 +1,11 @@
 ﻿using Autofac;
+using Prism.Commands;
 using Prism.Events;
 using System;
+using System.Windows;
+using System.Windows.Input;
+using Wider.Core;
+using Wider.Core.Controls;
 using Wider.Core.Services;
 
 namespace WiderClipboard.Models
@@ -10,7 +15,66 @@ namespace WiderClipboard.Models
         public Workspace(IContainer container, IEventAggregator eventAggregator) : base(container, eventAggregator)
         {
             Title = "Wider Clipboard Viewer";
+
+            LoadCommand();
+            LoadMenu();
         }
 
+
+        private void LoadCommand()
+        {
+            ICommandManager commandManager = _container.Resolve<ICommandManager>();
+
+            ICommand refreshCommand = new DelegateCommand(RefreshCommand);
+            ICommand exitCommand = new DelegateCommand(() => App.Current.MainWindow.Close());
+
+            commandManager.RegisterCommand("RefreshCommand", refreshCommand);
+            commandManager.RegisterCommand("ExitCommand", exitCommand);
+        }
+
+        private void RefreshCommand()
+        {
+            // Refresh Menu items
+            ICommandManager commandManager = _container.Resolve<ICommandManager>();
+            IMenuService menuService = _container.Resolve<IMenuService>();
+
+            MenuItemViewModel item = menuService.Get("_Clipboard") as MenuItemViewModel;
+            foreach (AbstractCommandable child in item.Children)
+            {
+                item.Remove(child.GuidString);
+            }
+
+            Int32 index = 300;
+            foreach (String name in Clipboard.GetDataObject().GetFormats(false))
+            {
+                item.Add(new MenuItemViewModel(name, ++index, null, 
+                    commandManager.GetCommand("ExitCommand")));
+            }
+
+            menuService.Refresh();
+        }
+
+        private void LoadMenu()
+        {
+            ICommandManager commandManager = _container.Resolve<ICommandManager>();
+            IMenuService menuService = _container.Resolve<IMenuService>();
+
+            // Add file command
+            menuService.Add(new MenuItemViewModel("_File", 1));
+
+            menuService.Get("_File")
+                .Add(new MenuItemViewModel("E_xit", 101, null,
+                    commandManager.GetCommand("ExitCommand"),
+                    new KeyGesture(Key.F4, ModifierKeys.Alt, "Alt + F4")));
+
+            // Add refresh command
+            menuService.Add(new MenuItemViewModel("_Refersh", 2, null,
+                commandManager.GetCommand("RefreshCommand"),
+                new KeyGesture(Key.F5)));
+
+            // Clipboard items
+            menuService.Add(new MenuItemViewModel("_Clipboard", 3));
+
+        }
     }
 }
