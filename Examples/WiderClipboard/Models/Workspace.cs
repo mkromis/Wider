542bc5102587e97c30ce02c46ae3e71b6cba9e40
Mark Kromis <mkromis@outlook.com>
2018-08-21 19:41:46 -0400

    Display window with string data.

--- Examples/WiderClipboard/Models/Workspace.cs
+++ Examples/WiderClipboard/Models/Workspace.cs
@@ -2,6 +2,7 @@
 using Prism.Commands;
 using Prism.Events;
 using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
 using Wider.Core;
@@ -29,7 +30,7 @@ namespace WiderClipboard.Models
             ICommand refreshCommand = new DelegateCommand(RefreshCommand);
             ICommand updateClipboard = new DelegateCommand<String>(UpdateDocument);
             ICommand exitCommand = new DelegateCommand(() => App.Current.MainWindow.Close());
-            
+
             commandManager.RegisterCommand("RefreshCommand", refreshCommand);
             commandManager.RegisterCommand("UpdateCommand", updateClipboard);
             commandManager.RegisterCommand("ExitCommand", exitCommand);
@@ -66,7 +67,7 @@ namespace WiderClipboard.Models
 
             toolbarService.Add(new ToolbarViewModel("Standard", 1) { Band = 1, BandIndex = 1 });
             toolbarService.Get("Standard").Add(menuService.Get("_Refersh"));
-            toolbarService.Get("Standard").Add(new MenuItemViewModel("Clipboard", 2));
+            toolbarService.Get("Standard").Add(new MenuItemViewModel("Clipboard", 201));
         }
 
         // Menu command to refersh the cliboard command
@@ -92,24 +93,33 @@ namespace WiderClipboard.Models
                     CommandParameter = name
                 });
 
-                toolbarService.Get("Standard").Get("Clipboard").Add(new MenuItemViewModel(name, ++index, null, commandManager.GetCommand("UpdateCommand")) { CommandParameter = name });
+                toolbarService.Get("Standard").Get("Clipboard").Add(
+                    new MenuItemViewModel(name, ++index, null, commandManager.GetCommand("UpdateCommand"))
+                    {
+                        CommandParameter = name
+                    });
             }
-
             menuService.Refresh();
         }
 
         // Show the document for clipboard strings.
         private void UpdateDocument(String format)
         {
-            Object data = Clipboard.GetData(format);
-
-            switch (data)
+            try
             {
-                case String stringData:
-                    Documents.Add(StringOutputViewModel.Create(_container, format, stringData));
-                    break;
+                Object data = Clipboard.GetData(format);
+
+                switch (data)
+                {
+                    case String stringData:
+                        Documents.Add(StringOutputViewModel.Create(_container, format, stringData));
+                        break;
+                }
+            }
+            catch (COMException e)
+            {
+                Documents.Add(StringOutputViewModel.Create(_container, format, e.Message));
             }
         }
-
     }
 }
