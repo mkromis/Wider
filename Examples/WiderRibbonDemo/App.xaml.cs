7df1862db9d7419f3580340e7b19de4c05a1a345
Mark Kromis <mkromis@outlook.com>
2018-10-25 21:10:04 -0400

    Create basic Ribbon Demo

---
+++ Examples/WiderRibbonDemo/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using WiderRibbonDemo.Views;
+using Prism.Modularity;
+using System.Windows;
+
+namespace WiderRibbonDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
+    }
+}
