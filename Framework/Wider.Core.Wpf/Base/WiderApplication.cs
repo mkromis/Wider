6e7ef5d232bfa8d9fd7aa381f83c0f7a49ed5c42
Mark Kromis <mkromis@outlook.com>
2018-10-28 00:56:06 -0400

    First attempt at converting to Prism.DryIoc 7.1

---
+++ Framework/Wider.Core.Wpf/Base/WiderApplication.cs
@@ -0,0 +1,55 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Wider.Core.Services;
+
+namespace Wider.Core
+{
+    public class WiderApplication : PrismApplication
+    {
+        CoreModule coreModule;
+
+        protected override Window CreateShell() {
+            // now we should have a shell, load settings and show if we can.
+            IShell shell = Container.Resolve<IShell>();
+            coreModule.LoadSettings();
+
+            // Assign main window object and show window.
+            Window window = shell as Window;
+            window.DataContext = Container.Resolve<IWorkspace>();
+            return window;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+            // Load core module, this is internal so call manually
+            coreModule = Container.Resolve<CoreModule>();
+            coreModule.RegisterTypes(containerRegistry);
+            coreModule.OnInitialized(Container);
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.Initialize();
+
+            // Check for existance, make sure we init first.
+            // load from disk in case of it not showing up in module catalog.
+            // resolving seems to init properly.
+            if (System.IO.File.Exists("Wider.Splash.dll"))
+            {
+                // Assume exits
+                Assembly assembly = Assembly.LoadFrom("Wider.Splash.dll");
+                Type type = assembly.GetType("Wider.Splash.SplashModule");
+                IModule splash = (IModule)Container.Resolve(type);
+                splash.OnInitialized(Container);
+            }
+            base.ConfigureModuleCatalog(moduleCatalog);
+        }
+    }
+}
--- Framework/Wider.Core.Wpf/Base/WiderBootstrapper.cs
+++ Framework/Wider.Core.Wpf/Base/WiderBootstrapper.cs
@@ -1,17 +1,23 @@
 ﻿#region License
 
+// Copyright (c) 2018 Mark Kromis
 // Copyright (c) 2013 Chandramouleswaran Ravichandran
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+/// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // 
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #endregion
 
-using Autofac;
-using Prism.Autofac;
+using DryIoc;
+using Prism.DryIoc;
+using Prism.Ioc;
 using Prism.Modularity;
 using System;
 using System.Linq;
@@ -24,11 +30,12 @@ using Wider.Core.Views;
 
 namespace Wider.Core
 {
-    public class WiderBootstrapper : AutofacBootstrapper
+    public class WiderBootstrapper : DryIocBootstrapper
     {
         //If you want your own splash window - inherit from the bootstrapper and register type ISplashView
         protected override void InitializeModules()
         {
+            IContainerExtension container = CreateContainerExtension();
             ModuleCatalog.Initialize();
 
             // Check for existance, make sure we init first.
@@ -40,12 +47,12 @@ namespace Wider.Core
                 Assembly assembly = Assembly.LoadFrom("Wider.Splash.dll");
                 Type type = assembly.GetType("Wider.Splash.SplashModule");
                 IModule splash = (IModule)Container.Resolve(type);
-                splash.Initialize();
+                splash.OnInitialized(container);
             }
 
             // Load core module, this is internal so call manually
             CoreModule coreModule = Container.Resolve<CoreModule>();
-            coreModule.Initialize();
+            coreModule.OnInitialized(container);
 
             base.InitializeModules();
 
