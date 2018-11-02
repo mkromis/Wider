06048f294e03edef03d10b0daeea552fcf9c0235
Mark Kromis <mkromis@outlook.com>
2018-11-01 20:57:26 -0400

    Upgrade ribbons to prism 7

--- Modules/Wider.Shell.Ribbon/RibbonModule.cs
+++ Modules/Wider.Shell.Ribbon/RibbonModule.cs
@@ -1,23 +1,18 @@
-﻿using Wider.Shell.Ribbon.Views;
+﻿using Prism.Ioc;
 using Prism.Modularity;
-using Prism.Regions;
 
 namespace Wider.Shell.Ribbon
 {
     public class RibbonModule : IModule
     {
-        public void Initialize()
+        public void OnInitialized(IContainerProvider containerProvider)
         {
+
         }
 
-        //public void OnInitialized(IContainerProvider containerProvider)
-        //{
- 
-        //}
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
 
-        //public void RegisterTypes(IContainerRegistry containerRegistry)
-        //{
-            
-        //}
+        }
     }
 }
