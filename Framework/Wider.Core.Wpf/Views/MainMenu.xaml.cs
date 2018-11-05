1f5c9d519d5ca8818cf70485c3e3dc16e7fe2f99
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2018-11-04 23:09:32 -0500

    Add Main menu as own region

---
+++ Framework/Wider.Core.Wpf/Views/MainMenu.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using Wider.Core.Controls;
+using Wider.Core.Services;
+
+namespace Wider.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+
+        /// <summary>
+        /// The menu service
+        /// </summary>
+        protected MenuItemViewModel _menus;
+
+
+        public MainMenu(IMenuService menuService)
+        {
+            InitializeComponent();
+            DataContext = menuService;
+        }
+    }
+}
