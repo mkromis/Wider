29648a9b22b42a41dd22301327c039ecd8b30184
Mark Kromis <mkromis@outlook.com>
2018-11-08 21:59:24 -0500

    Initial ribbon test with using region from project

--- Examples/WiderRibbonDemo/App.xaml.cs
+++ Examples/WiderRibbonDemo/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Regions;
 using Wider.Core;
+using WiderRibbonDemo.Views;
 
 namespace WiderRibbonDemo
 {
@@ -8,5 +11,15 @@ namespace WiderRibbonDemo
     /// </summary>
     public partial class App : WiderApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            base.RegisterTypes(containerRegistry);
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Container.Resolve<IRegionManager>().RegisterViewWithRegion("MainRibbon", typeof(MainRibbon));
+        }
     }
 }
