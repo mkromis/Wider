64ba37d87d3e2b0bc1f7b31cf185e0736e99a3bc
Mark Kromis <mkromis@outlook.com>
2018-12-08 21:17:41 -0500

    Change namespace of sample gestures

--- Modules/Wider.Content.VirtualCanvas/Gestures/AutoScroll.cs
+++ Modules/Wider.Content.VirtualCanvas/Gestures/AutoScroll.cs
@@ -11,7 +11,7 @@ using System.Windows.Shapes;
 using System.Windows.Media;
 using System.Windows.Input;
 
-namespace Microsoft.Sample.Controls
+namespace Wider.Content.VirtualCanvas.Gestures
 {
     /// <summary>
     /// This class implements a mouse middle-button auto-scrolling feature over any target view.
