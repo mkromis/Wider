19473dac146cd54c5e90d59242d4d7d0c286fcbd
Mark Kromis <mkromis@outlook.com>
2019-01-07 20:36:17 -0500

    Change _container to Container Add TaskRun test Add title to virtual canvas

---
+++ Examples/WiderRibbonDemo/Views/TaskRunTests.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using Wider.Core.Services;
+
+namespace WiderRibbonDemo.Views
+{
+    /// <summary>
+    /// Interaction logic for TaskRunTests
+    /// </summary>
+    public partial class TaskRunTests : UserControl, IContentView
+    {
+        public TaskRunTests()
+        {
+            InitializeComponent();
+        }
+    }
+}
