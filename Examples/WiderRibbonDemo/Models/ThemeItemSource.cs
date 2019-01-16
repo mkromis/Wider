9f33781c39411c787c7ee9810943de4d92e93f02
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-01-15 21:57:38 -0500

    Add sample theme switcher

---
+++ Examples/WiderRibbonDemo/Models/ThemeItemSource.cs
@@ -0,0 +1,18 @@
+﻿using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
+
+namespace WiderRibbonDemo.Models
+{
+    internal class ThemeItemSource : IItemsSource
+    {
+        public ItemCollection GetValues()
+        {
+            return new ItemCollection
+            {
+                "Default",
+                "Light",
+                "Dark",
+                "Blue",
+            };
+        }
+    }
+}
