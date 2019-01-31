de20a4ada167e7d4d2f1e92c8285ade757d53777
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-01-31 14:53:13 -0500

    HexEditor not releasing memory on close.

--- Examples/WiderClipboard/Views/HexViewer.xaml.cs
+++ Examples/WiderClipboard/Views/HexViewer.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using Wider.Core.Services;
 
 namespace WiderClipboard.Views
@@ -6,11 +8,20 @@ namespace WiderClipboard.Views
     /// <summary>
     /// Interaction logic for HexViewer
     /// </summary>
-    public partial class HexViewer : IContentView
+    public partial class HexViewer : IContentView, IDisposable
     {
         public HexViewer()
         {
             InitializeComponent();
+        }
+
+        public void Dispose()
+        {
+            editor.Dispose();
+            editor = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
         }
     }
 }
