c9e432b3e90f17953bd11a8052b60ef204fdf67c
Mark Kromis <mkromis@outlook.com>
2019-01-30 22:22:38 -0500

    Initial hex editor view working

---
+++ Examples/WiderClipboard/ViewModels/HexViewerViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Ioc;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Wider.Core.Services;
+using WiderClipboard.Models;
+using WiderClipboard.Views;
+
+namespace WiderClipboard.ViewModels
+{
+    public class HexViewerViewModel : ContentViewModel
+    {
+        private MemoryStream _memoryStream;
+
+        public HexViewerViewModel(IContainerExtension container) : base(container)
+        {
+            Model = new EmptyModel();
+            View = new HexViewer();
+        }
+
+        public MemoryStream MemoryStreamItem {
+            get => _memoryStream;
+            set => SetProperty(ref _memoryStream, value);
+        }
+    }
+}
