b8f0af1653ccf1613267889121ebb6eb4fd41bf2
Mark Kromis <mkromis@outlook.com>
2019-02-12 20:37:04 -0500

    Move tests to test folder

---
+++ Tests/Wider.SplashTests/SplashModuleTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wider.Splash;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wider.Splash.Tests
+{
+    [TestClass()]
+    public class SplashModuleTests
+    {
+        [TestMethod()]
+        public void SplashModuleTest()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod()]
+        public void RegisterTypesTest()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod()]
+        public void OnInitializedTest()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
